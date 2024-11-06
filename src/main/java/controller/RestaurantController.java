27677830468a24dfe65a59466aabe576d7f23cf5
henryjaehun <noframed@gmail.com>
2024-11-06 20:26:17 +0900

    under rebuilding source - restaurant register

--- src/main/java/controller/RestaurantController.java
+++ src/main/java/controller/RestaurantController.java
@@ -3,18 +3,21 @@ package controller;
 import dto.RestaurantFormDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import service.RestaurantService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/restaurant")
 public class RestaurantController {
 
     private final RestaurantService restaurantService;
 
-    @PostMapping("restaurant/register")
-    public String createRestaurant() {
-        Long id = restaurantService.registerRestaurant(new RestaurantFormDto());
+    @PostMapping("/register")
+    public String createRestaurant(@RequestBody RestaurantFormDto restaurantFromDto) {
+        Long id = restaurantService.registerRestaurant(restaurantFromDto);
         return "Restaurant created: " + id;
     }
 
